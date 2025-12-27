fe0e4cf32db43eec0a8600b67274a0c62163a15a
DievselMaestro <121482364+DievselMaestro@users.noreply.github.com>
2025-12-27 16:59:58 +0100

    Funktion mehere RSS-Feed können abgefragt werden

--- RSS-Reader.py
+++ RSS-Reader.py
@@ -13,15 +13,36 @@ def is_var_empty(var):
 
 # Funktion für die Eingabe der URL anfragen
 def prompt_input():
-    print("Enter RSS feed URL")
-    rss_feed_url = input()
-    return (rss_feed_url)
+    #Debug On or Off
+    var_debug_on = 0
+    if var_debug_on == 1:
+        rss_feed_url = "https://www.heise.de/rss/heise-atom.xml"
+    else:
+        print("Enter RSS feed URL:")
+        rss_feed_url = input()
+        
+    return rss_feed_url
+               
 
 def get_rss_feed():
+    # RSS von der URL Adresse abrufen
     d = feedparser.parse(prompt_input())
-    print(d.feed.title_detail)
-
-
+    # print(d.feed.title_detail)
+    
+    i = 0
+    for x in d.entries:
+        #print(x)
+        # RSS feed
+        print("RSS Feed:",i)
+        print("")
+        print("Titel: ",d.entries[i].title)
+        print("Link:", d.entries[i].link)
+        print("Beschreibung:",d.entries[i].description)
+        print("------------------")
+        print("")
+        i = i + 1
+        #print("Datum:", d.feed.published)
+        
 
 def main():
     get_rss_feed()
@@ -36,4 +57,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-print(__name__)
+#print(__name__)
