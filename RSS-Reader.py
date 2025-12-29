350ad970318a9f87376e6bd7fb2dd4036718acc9
DievselMaestro <121482364+DievselMaestro@users.noreply.github.com>
2025-12-29 10:06:50 +0100

    Code Verbesserungen 3 und 4 umgesetzt.

--- RSS-Reader.py
+++ RSS-Reader.py
@@ -5,19 +5,19 @@ init(autoreset=True)
 
 # Check is the Variabel is empty or not
 # for Debug reason
-def is_var_empty(var):
-    if var == "":
-        print("The Var:",var,"is empty!")
-    else:
-        print("The Var:",var,"is not empty!")
-        var_is_not_empty = True
-        return(var_is_not_empty)
+# def is_var_empty(var):
+#     if var == "":
+#         print("The Var:",var,"is empty!")
+#     else:
+#         print("The Var:",var,"is not empty!")
+#         var_is_not_empty = True
+#         return(var_is_not_empty)
 
 # Funktion für die Eingabe der URL anfragen
 def prompt_input():
     # Debug On or Off
     var_debug_on: bool = True
-    if var_debug_on == True:
+    if var_debug_on:
         rss_feed_url: str = "https://www.heise.de/rss/heise-atom.xml"
     else:
         print("Enter RSS feed URL:")
@@ -31,22 +31,18 @@ def get_rss_feed():
     d = feedparser.parse(prompt_input())
     # print(d.feed.title_detail)
     
-    i = 0
-    for x in d.entries:
-        #print(x)
+    
+    for index, entry in enumerate(d.entries):
         # RSS feed
-        print(Style.BRIGHT + Fore.YELLOW + "RSS Feed:",i)
+        print(Style.BRIGHT + Fore.YELLOW + "RSS Feed:",index)
         print("")
-        print(Style.BRIGHT + Fore.MAGENTA + "Titel: ",d.entries[i].title)
-        print(Style.BRIGHT + Fore.MAGENTA + "Beschreibung:",d.entries[i].description)
-        print(Style.BRIGHT + Fore.MAGENTA + "Link:", d.entries[i].link)
-        print(Style.DIM + Fore.BLUE + "Datum:", Fore.BLUE + d.entries[i].published)
+        print(Style.BRIGHT + Fore.MAGENTA + "Titel: ",entry.title)
+        print(Style.BRIGHT + Fore.MAGENTA + "Beschreibung:",entry.description)
+        print(Style.BRIGHT + Fore.MAGENTA + "Link:", entry.link)
+        print(Style.DIM + Fore.BLUE + "Datum:", Fore.BLUE + entry.published)
         print("")
         print("------------------")
         print("")
-        i = i + 1
-        #print("Datum:", d.feed.published)
-        
  
 def main():
     get_rss_feed()
